2d0c5f865b13831f61c5664d2cb7ad9b0fcbcb5d
Das <athuldas@hamon.in>
2023-01-09 18:39:15 +0530

    Adds some more dummy testcases.

--- app/calculations.py
+++ app/calculations.py
@@ -12,3 +12,23 @@ def mul(num1: int, num2: int):
 
 def div(num1: int, num2: int):
     return num1 / num2
+
+
+class InsufficientFunds(Exception):
+    pass
+
+
+class BankAccount:
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientFunds("Insufficient funds in your account")
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add, sub, mul
+from app.calculations import add, BankAccount, InsufficientFunds
 
 
 @pytest.mark.parametrize(
@@ -14,3 +14,55 @@ from app.calculations import add, sub, mul
 )
 def test_add(num1, num2, expected):
     assert add(num1, num2) == expected
+
+
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
+def test_bank_set_initial_balance(bank_account):
+    assert bank_account.balance == 50
+
+
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
+
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(30)
+    assert bank_account.balance == 20
+
+
+def test_deposit(bank_account):
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance) == 55
+
+
+@pytest.mark.parametrize(
+    "deposited,withdraw,expected",
+    [
+        (500, 200, 300),
+        (200, 200, 0),
+        (300, 200, 100),
+    ],
+)
+def test_bank_transaction(zero_bank_account, deposited, withdraw, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdraw)
+    assert zero_bank_account.balance == expected
+
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(100)
