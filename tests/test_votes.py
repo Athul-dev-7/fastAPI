d5e71864b99978f6a2cc1e17d38d00b8fe6236f4
Das <athuldas@hamon.in>
2023-01-15 11:24:13 +0530

    Adds test cases for vote route.

---
+++ tests/test_votes.py
@@ -0,0 +1,47 @@
+import pytest
+from app import models
+
+
+@pytest.fixture
+def test_vote(test_posts, session, test_user_one):
+    new_vote = models.Vote(post_id=test_posts[0].id, user_id=test_user_one["id"])
+    session.add(new_vote)
+    session.commit()
+
+
+def test_vote_on_post(authorized_client, test_posts):
+    response = authorized_client.post(
+        "/vote", json={"post_id": test_posts[0].id, "vote_dir": 1}
+    )
+    assert response.status_code == 201
+
+
+def test_vote_twice_on_post(authorized_client, test_posts, test_vote):
+    response = authorized_client.post(
+        "/vote", json={"post_id": test_posts[0].id, "vote_dir": 1}
+    )
+    assert response.status_code == 409
+
+
+def test_delete_vote(authorized_client, test_posts, test_vote):
+    response = authorized_client.post(
+        "/vote", json={"post_id": test_posts[0].id, "vote_dir": 0}
+    )
+    assert response.status_code == 201
+
+
+def test_delete_vote_non_exist(authorized_client, test_posts):
+    response = authorized_client.post(
+        "/vote", json={"post_id": test_posts[0].id, "vote_dir": 0}
+    )
+    assert response.status_code == 404
+
+
+def test_vote_post_non_exist(authorized_client):
+    response = authorized_client.post("/vote", json={"post_id": 877878, "vote_dir": 1})
+    assert response.status_code == 404
+
+
+def test_vote_unauthorized_user(client, test_posts):
+    response = client.post("/vote", json={"post_id": test_posts[0].id, "vote_dir": 1})
+    assert response.status_code == 401
