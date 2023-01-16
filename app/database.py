b03396969cc3301204003849a34f9170d2439d43
Das <athuldas@hamon.in>
2023-01-16 10:43:55 +0530

    Hardcoded port number.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://${{secrets.DATABASE_USERNAME}}:${{secrets.DATABASE_PASSWORD}}@${{secrets.DATABASE_HOST_NAME}}:${{secrets.DATABASE_PORT}}/${{secrets.DATABASE_NAME}}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://${{secrets.DATABASE_USERNAME}}:${{secrets.DATABASE_PASSWORD}}@${{secrets.DATABASE_HOST_NAME}}:5432/${{secrets.DATABASE_NAME}}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
