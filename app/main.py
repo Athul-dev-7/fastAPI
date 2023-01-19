23bc0b409a75f0ce2a5a4c664496b5365ea34156
Das <athuldas@hamon.in>
2023-01-19 12:35:55 +0530

    Adds "try it out" for /redoc.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse
 
 from .routers import posts, users, auth, vote
 
 # No need to bind if alembic is installed
 # models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(redoc_url=None)
 
 origins = ["*"]
 
@@ -27,3 +28,57 @@ app.include_router(vote.router)
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+def get_redoc_html(
+    *,
+    openapi_url: str,
+    title: str,
+    redoc_favicon_url: str = "https://fastapi.tiangolo.com/img/favicon.png",
+    with_google_fonts: bool = True,
+):
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>{title}</title>
+    <!-- needed for adaptive design -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    """
+    if with_google_fonts:
+        html += """
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+    """
+    html += f"""
+        <link rel="shortcut icon" href="{redoc_favicon_url}">
+        <!--
+        ReDoc doesn't change outer page styles
+        -->
+        <style>
+          body {{
+            margin: 0;
+            padding: 0;
+          }}
+        </style>
+        </head>
+        <body>
+            <div id="redoc-container"></div>
+              <script src="https://cdn.jsdelivr.net/npm/redoc@2.0.0-rc.55/bundles/redoc.standalone.min.js"> </script>
+              <script src="https://cdn.jsdelivr.net/gh/wll8/redoc-try@1.4.1/dist/try.js"></script>
+              <script>
+                initTry({{
+                openApi: `{openapi_url}`,
+                  redocOptions: {{scrollYOffset: 50}},
+                }})
+              </script>
+        </body>
+        </html>
+        """
+    return HTMLResponse(html)
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_try_it_out():
+    title = app.title + " Redoc with try it out"
+    return get_redoc_html(openapi_url=app.openapi_url, title=title)
